81f3dfb19bc1c6903a4754dabd19462dc1c6ef9a
Nirvana16 <zedanilo161@gmail.com>
2017-10-27 17:26:10 -0200

    Aplicado o Thymeleaf na View

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.Titulos;
@@ -54,12 +55,12 @@ public class TituloController {
 	 */
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public String salvar(Titulo titulo) {
-		titulos.save(titulo);			
+	public ModelAndView salvar(Titulo titulo) {
+		titulos.save(titulo);	
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagem", "Titulo Salvo com Sucesso");
 		
-		System.out.println(titulo.getDescricao() + " salvo no bd com sucesso");
-		
-		return "CadastroTitulo";
+		return mv;
 	}
 
 }
