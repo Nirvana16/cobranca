a687adc1fa2a294a0272395857b1d95dda84f1ce
Danilo Azevedo <zedanilo161@gmail.com>
2017-11-04 00:28:32 -0200

    2.15 implementando a edicao v1

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -29,6 +30,14 @@ import com.algaworks.cobranca.repository.Titulos;
 public class TituloController {
 	
 	/**
+	 * como estamos repetindo o tempo todo CadastroTitulo, 
+	 * criamos uma varaivel final com o valor da string, assim
+	 * facilita a vida.
+	 */
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
+	
+	
+	/**
 	 * O @Autowired serve para indicar de onde esta vindo
 	 * o objeto que será passado para dentro dos metodos do controle 
 	 * referente ao private Titulos titulos; sem a notação receberiamos
@@ -39,7 +48,7 @@ public class TituloController {
 	
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		/**
 		 * mv.addObject("TodosStatusTitulo", StatusTitulo.values());
 		 * Retorna um array com todos os Enun's presentes na interface
@@ -107,7 +116,7 @@ public class TituloController {
 			/*
 			 * Retorna o nome da pagina HTML
 			 */
-			return "CadastroTitulo";
+			return CADASTRO_VIEW;
 		}		
 		
 		titulos.save(titulo);			
@@ -151,6 +160,47 @@ public class TituloController {
 		List<Titulo> todosTitulos = titulos.findAll();
 		ModelAndView mv = new ModelAndView("PesquisaTitulos");
 		mv.addObject("titulos", todosTitulos);
+		return mv;
+	}
+	
+	/*
+	 * @RequestMapping("{codigo}")
+	 * Faz com que a requisição chegue com o código do item a ser editado
+	 * como o codigo é variavel usamos o {codigo}.
+	 * 
+	 * dessa forma podemos dizer para o metodo que ele ira receber
+	 * um codigo do tipo LONG sendo que é referente a um PathVariable
+	 * ou seja uma variavel que indica um caminho, quase um redirecionamento
+	 * @PathVariable Long codigo 
+	 * 
+	 * ja vou explicar o pq botei Titulo titulo
+	 */
+	@RequestMapping("{codigo}")
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) {
+		/*
+		 * Esse método findOne recupera o titulo de um 
+		 * determinado ID (codigo) que foi passado para ele
+		 * 
+		 * 	Titulo titulo = titulos.findOne(codigo);	
+		 * Comentei esse metodo pois o spring é tao sinistro
+		 * que se voce fizer Titulo titulo. ou seja 
+		 * se voce passar o objeto ele ja sabe que o codigo 
+		 * q ta vindo na PathVariabel é para ele buscar 
+		 * o objeto referenciado a ele...
+		 * voce so precisa dizer pra ele qual o nome
+		 * do parametro a buscar ("codigo")
+		 * 
+		 */		
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		/**
+		 * Com o mv.addObject(titulo); nós vamos retornar 
+		 * o titulo que foi recuperado no metodo findOne
+		 * assim ao contrario do que ocorre no metodo novo()
+		 * onde é retornado um novo Model and view
+		 * aqui retornamos o objeto que referenciado a ID 
+		 * a qual foi recebida
+		 */
+		mv.addObject(titulo);
 		return mv;
 	}
 	
